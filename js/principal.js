ed09b4a88b933e6cecf77d9208e8fdb80640c5f9
estefy <estefy@loadingplay.com>
2015-11-17 01:10:59 -0300

    se cambia storage por local

--- js/principal.js
+++ js/principal.js
@@ -28,9 +28,9 @@ $(document).ready(function(){
 	});
 
 	$("#form-contact").submit(function(){
-		sessionStorage.setItem("carrera", $("#carrera").val());
-		sessionStorage.setItem("contacto", $("#contacto").val());
-		sessionStorage.setItem("mensaje", $("#mensaje").val());
+		localStorage.setItem("carrera", $("#carrera").val());
+		localStorage.setItem("contacto", $("#contacto").val());
+		localStorage.setItem("mensaje", $("#mensaje").val());
 	});
 
 	// $("#form-perfil")submit(function(){
@@ -45,32 +45,32 @@ $(document).ready(function(){
 	// });
 
 	$(".enviar").click(function(){
-		sessionStorage.setItem("nombre", $("#nombre-perfil").val());
-		sessionStorage.setItem("rut", $("#rut-perfil").val());
-		sessionStorage.setItem("carrera", $("#carrera-perfil").val());
-		sessionStorage.setItem("seccion", $("#seccion").val());
-		sessionStorage.setItem("nivel", $("#nivel").val());
-		sessionStorage.setItem("direccion", $("#direccion").val());
-		sessionStorage.setItem("comuna", $("#comuna").val());
-		sessionStorage.setItem("numero-e", $("#numero-e").val());
+		localStorage.setItem("nombre", $("#nombre-perfil").val());
+		localStorage.setItem("rut", $("#rut-perfil").val());
+		localStorage.setItem("carrera", $("#carrera-perfil").val());
+		localStorage.setItem("seccion", $("#seccion").val());
+		localStorage.setItem("nivel", $("#nivel").val());
+		localStorage.setItem("direccion", $("#direccion").val());
+		localStorage.setItem("comuna", $("#comuna").val());
+		localStorage.setItem("numero-e", $("#numero-e").val());
 	});
 
-	
 
 
 
-    $(".carrera-selec").html(sessionStorage.getItem('carrera'));
-    $(".contacto-selec").html(sessionStorage.getItem('contacto'));
-    $(".mensaje-selec").html(sessionStorage.getItem('mensaje'));
 
-    $(".nombre-selec").html(sessionStorage.getItem('nombre'));
-    $(".rut-selec").html(sessionStorage.getItem('rut'));
-    $(".carrera-selec").html(sessionStorage.getItem('carrera'));
-    $(".nivel-selec").html(sessionStorage.getItem('nivel'));
-    $(".seccion-selec").html(sessionStorage.getItem('seccion'));
-    $(".direccion-selec").html(sessionStorage.getItem('direccion'));
-    $(".comuna-selec").html(sessionStorage.getItem('comuna'));
-    $(".numero-e-selec").html(sessionStorage.getItem('numero-e'));
+    $(".carrera-selec").html(localStorage.getItem('carrera'));
+    $(".contacto-selec").html(localStorage.getItem('contacto'));
+    $(".mensaje-selec").html(localStorage.getItem('mensaje'));
+
+    $(".nombre-selec").html(localStorage.getItem('nombre'));
+    $(".rut-selec").html(localStorage.getItem('rut'));
+    $(".carrera-selec").html(localStorage.getItem('carrera'));
+    $(".nivel-selec").html(localStorage.getItem('nivel'));
+    $(".seccion-selec").html(localStorage.getItem('seccion'));
+    $(".direccion-selec").html(localStorage.getItem('direccion'));
+    $(".comuna-selec").html(localStorage.getItem('comuna'));
+    $(".numero-e-selec").html(localStorage.getItem('numero-e'));
 
 });
 
